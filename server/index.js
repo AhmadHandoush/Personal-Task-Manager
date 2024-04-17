97b16d1e1b1c051756c67ee361804453dc3456b1
hamada <hamadahandoush9991@gmail.com>
2024-04-17 03:12:32 +0200

    backend

--- server/index.js
+++ server/index.js
@@ -1,31 +1,17 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const User = require("./models/User.model");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./Routes/userRoutes");
+const boardRoutes = require("./Routes/boardRoutes");
+
+dotenv.config();
+connectDB();
+
 const app = express();
 app.use(express.json());
-app.use(cors());
-mongoose.connect("mangodb://127.0.0.1:27017/task");
 
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  User.findOne({ email: email }).then((user) => {
-    if (user) {
-      if (user.password === password) {
-        res.json("Success");
-      } else {
-        res.json("the password is incorrect");
-      }
-    } else {
-      res.json("user not exist");
-    }
-  });
-});
-app.post("/register", (req, res) => {
-  User.create(req.body)
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
-});
-app.listen(3001, () => {
-  console.log("connect ");
-});
+app.use("/api/users", userRoutes);
+app.use("/api/boards", boardRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
